dc19c38987ec259b7d77b0af3bc22ad43e627542
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-05-15 23:07:25 +0100

    added validates for whole form

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,3 +1,6 @@
 class Computer < ApplicationRecord
+  validates :name, presence: true;
+  validates :available, presence: true;
+  validates :color, presence: true;
   validates :serial_number, format: /\A[a-zA-Z0-9]*\z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true
   validates :email, presence: true, format: /\w+@spartaglobal.com/
+  validates :address, presence: true
 end
